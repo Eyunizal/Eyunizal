6c71d4d3583b13d37b8b8eedfa1247b6f3316701
Eyunizal <emilio.yunizal@gmail.com>
2018-05-04 17:53:42 +0700

    bootstrap

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+// Perkalian //
+namaLengkap = prompt("nama?");
+angkaPertama = prompt("input angka pertama");
+angkaKedua = prompt("input angka kertama");
+hasil = angkaPertama * angkaKedua;
+
+alert("Halo " + namaLengkap);
+alert("Hasil perkalian adalah " + hasil);
---
+++ test.js
@@ -0,0 +1,13 @@
+var level = 3;
+var angka = 1;
+
+for (var baris = 0; baris < level; baris++) 
+{
+    var result = '';
+    for (var kolom = 0; kolom < level; kolom++ ) 
+    {
+        result = resut + angka + 'x';
+        angka++;
+    }
+  console.log (xo);
+} 
